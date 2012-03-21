28a9d8cae2bbf2f1645222fd17285bdcfab85aad
Bob Carroll <bob.carroll@alum.rit.edu>
2012-03-20 20:20:21 -0700

    part of the switch to multiple source files in a distribution

---
+++ libfusion/SourceFile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Fusion.Framework
+{
+    /// <summary>
+    /// Represents a source file that is on the local disk.
+    /// </summary>
+    public class SourceFile
+    {
+        private string _digest;
+        private string _localname;
+        private long _size;
+
+        /// <summary>
+        /// Initialises the local source.
+        /// </summary>
+        /// <param name="digest">expected digest of the file</param>
+        /// <param name="localname">file name to use when saving to distfiles</param>
+        /// <param name="size">size of the file in bytes</param>
+        internal SourceFile(string digest, string localname, long size)
+        {
+            _digest = digest;
+            _localname = localname;
+            _size = size;
+        }
+
+        /// <summary>
+        /// Expected digest of the file.
+        /// </summary>
+        public string Digest
+        {
+            get { return _digest; }
+        }
+
+        /// <summary>
+        /// File name to use when saving to distfiles.
+        /// </summary>
+        public string LocalName
+        {
+            get { return _localname; }
+        }
+
+        /// <summary>
+        /// Size of the file in bytes.
+        /// </summary>
+        public long Size
+        {
+            get { return _size; }
+        }
+    }
+}
---
+++ libfusion/WebSourceFile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Fusion.Framework
+{
+    /// <summary>
+    /// Represents a source file that is downloaded from the web.
+    /// </summary>
+    public sealed class WebSourceFile : SourceFile
+    {
+        private Uri _location;
+
+        /// <summary>
+        /// Initialises the web source.
+        /// </summary>
+        /// <param name="location">remote URL to download from</param>
+        /// <param name="digest">expected digest of the file</param>
+        /// <param name="localname">file name to use when saving to distfiles</param>
+        /// <param name="size">size of the file in bytes</param>
+        internal WebSourceFile(Uri location, string digest, string localname, long size)
+            : base(digest, localname, size)
+        {
+            _location = location;
+        }
+
+        /// <summary>
+        /// Fetch the file and put it in the given directory.
+        /// </summary>
+        /// <param name="destdir">destination directory</param>
+        public void Fetch(DirectoryInfo destdir)
+        {
+            try {
+                WebClient wc = new WebClient();
+                wc.DownloadFile(_location, destdir.FullName + @"\" + this.LocalName);
+            } catch (WebException ex) {
+                throw ex.InnerException;
+            }
+        }
+
+        /// <summary>
+        /// Remote URI to download from.
+        /// </summary>
+        public Uri Location
+        {
+            get { return _location; }
+        }
+    }
+}
