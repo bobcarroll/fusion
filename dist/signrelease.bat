fbf1425db559d3b0f2d726d255bcf35af0ef6be0
Bob Carroll <bob.carroll@alum.rit.edu>
2012-11-15 23:19:14 -0800

    added assembly strong name

---
+++ dist/signrelease.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\bootstrap.exe rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\fuse.exe rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\fusion-config.exe rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\lib7ztasks.dll rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\libfusion.dll rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\libfusiontasks.dll rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\libmsitasks.dll rcarz.snk
+"%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\x64\sn.exe" -R ..\bin\Release\xtmake.exe rcarz.snk
+
+pause
